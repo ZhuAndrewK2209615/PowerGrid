43359831526afa01553d93698eee204ece35ef2f
FanDaniel1 <k1416135@students.katyisd.org>
2026-04-01 07:41:30 -0500

    Refactor RoundManager to manage player order and phases

--- src/main/java/powergrid/managers/RoundManager.java
+++ src/main/java/powergrid/managers/RoundManager.java
@@ -1,5 +1,35 @@
 package powergrid.managers;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import powergrid.core.GameState;
+import powergrid.core.Player;
 public class RoundManager {
-    
+    private int Phases=1;
+    private static String currentPhase="Auction";
+    private static int Round=1;
+    private static ArrayList<Player> playerOrder = new ArrayList<>();
+}
+public static void determinePlayerOrder(){
+playerOrder = GameState.getPlayers();
+  Collections.sort(playerOrder);
+} 
+public ArrayList<Player> getPlayerOrder(){
+return playerOrder;
+}
+public static int advancePhase(int CurrentPhase) 
+    {
+if(CurrentPhase<4){
+CurrentPhase=CurrentPhase+1;
+}
+else{
+CurrentPhase=1;
+}
+
+return CurrentPhase;
+}
+public static int advanceRound(int CurrentRound){
+        CurrentRound=CurrentRound+1;
+        return CurrentRound;
+}
 }
