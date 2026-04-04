790802b262bbd6b1e3dd46049f979141fc0236dc
Rishi Khare <K2307266@students.katyisd.org>
2026-04-03 22:34:30 -0500

    checkpoint 4

--- src/main/java/powergrid/ui/marketPanel.java
+++ src/main/java/powergrid/ui/marketPanel.java
@@ -3,6 +3,7 @@ import javax.swing.JPanel;
 import javax.swing.event.MouseInputListener;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -16,7 +17,7 @@ import javax.swing.*;
 
 public class marketPanel extends JPanel implements MouseInputListener{
 
-    private BufferedImage GermanMap, market, playerPlant, coal, garbage, oil, uranium, rulesSymbol, currentAuction, rightArrow, leftArrow;
+    private BufferedImage background, GermanMap, market, playerPlant, coal, garbage, oil, uranium, rulesSymbol, currentAuction, rightArrow, leftArrow;
 
     public marketPanel(){
         try {
@@ -24,11 +25,12 @@ public class marketPanel extends JPanel implements MouseInputListener{
             market = ImageIO.read(getClass().getResource("/powergrid/Images/market.png"));
             rightArrow = ImageIO.read(getClass().getResource("/powergrid/Images/right-arrow.png"));
             leftArrow = ImageIO.read(getClass().getResource("/powergrid/Images/left-arrow.png"));
-            // will code player plant
-           coal = ImageIO.read(getClass().getResource("/powergrid/Images/coal.png"));
-           garbage = ImageIO.read(getClass().getResource("/powergrid/Images/Garbage.png"));
-            //oil = ImageIO.read(getClass().getResource("powergrid/Images/Oil.png"));
-           // uranium = ImageIO.read(getClass().getResource("powergrid/Images/Uranium.png"));
+            // will code player plant get image
+            coal = ImageIO.read(getClass().getResource("/powergrid/Images/coal.png"));
+            garbage = ImageIO.read(getClass().getResource("/powergrid/Images/Garbage.png"));
+            oil = ImageIO.read(getClass().getResource("/powergrid/Images/Oil.png"));
+            uranium = ImageIO.read(getClass().getResource("/powergrid/Images/uranium.png"));
+            background = ImageIO.read(getClass().getResource("/powergrid/Images/toChange.png"));
            // rulesSymbol = ImageIO.read(getClass().getResource("powergrid/Images/QuestionSymbol.png"));
            // currentAuction = ImageIO.read(getClass().getResource("powergrid/Images/energy.png"));
 
@@ -40,14 +42,55 @@ public class marketPanel extends JPanel implements MouseInputListener{
         addMouseListener(this); 
     }
 
+    public void paintComponent(Graphics g){  // do all background color changes here, do not touch the pain method
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+
+        g.setColor(new Color(255,250,191));
+        g.fillRect(924, 29, 945, 95);
+        g.fillRect(924, 153, 145, 50);
+        g.fillRect(1104, 150, 500, 55);
+        g.fillRect(1104, 150, 105, 55);
+        g.fillRect(1504, 150, 100, 55);
+        g.fillRect(1630, 150, 238, 55);
+        g.fillRect(920, 220, 950, 300);
+        g.fillRect(920,530, 470, 200);  // coal
+        g.fillRect(920+490, 530, 460, 200);  // oil
+        g.fillRect(920, 740, 470, 200);  // garbage
+        g.fillRect(920+490, 740, 460, 200); // uranium
+
+
+
+        g.setColor(Color.lightGray);
+        g.fillRect(935, 230, 290, 275);  // powerplant section 1
+        g.fillRect(1250, 230, 290, 275);  // powerplant section 2
+        g.fillRect(1565, 230, 290, 275);  // powerplant section 3
+        g.fillRect(1120,600,240,100);  // button to buy resource (changes color we need to code)
+        g.fillRect(1120,800,240,100);  // button to buy resource (changes color we need to code)
+        g.fillRect(1608,600,240,100);  // button to buy resource (changes color we need to code)
+        g.fillRect(1608,800,240,100);  // button to buy resource (changes color we need to code)
+
+
+
+
+
+        
+    }
+
     public void paint(Graphics g){
         super.paint(g);
-        g.drawImage(GermanMap, 0,0, getWidth()/2-148,1080,null);  // the map
+        g.drawImage(GermanMap, 0,0, getWidth()/2-148,getHeight(),null);  // the map
         g.drawImage(market, 800, 0, 100, 1050, null);  // resource market
 
         Graphics2D g2d = (Graphics2D)g;
         g2d.setStroke(new BasicStroke(7));
         g.drawRect(920, 25, 950, 100);  // top rectangle
+        g.setColor(Color.red);  // we have to change this
+        g2d.setFont(new Font("Arial", Font.BOLD, 33));
+        g.drawString("Player n", 1300, 190);
+        g.fillOval(960, 40, 75, 75);
+
+        g.setColor(Color.black);
         g.setFont(new Font("Arial", Font.BOLD, 30));
         g.drawString("Player " + "n " + "is buying resources", 1220, 65);   // define n
         g.drawString("(Next: player " + "n+1)", 1300, 100);  // define n+1
@@ -70,12 +113,15 @@ public class marketPanel extends JPanel implements MouseInputListener{
         g.setFont(new Font("Arial", Font.PLAIN, 24));
         g.drawString("Player Elecktros: 999", 1635, 185);   // define elecktros for player
 
-        g2d.setStroke(new BasicStroke(7));  // powerplant section --> not finished
+        g2d.setStroke(new BasicStroke(7));  // powerplant section
         g.drawRect(920, 220, 950, 300);
         g2d.setStroke(new BasicStroke(5));
         g.drawRect(935, 230, 290, 275);  // powerplant section 1
+        g.drawString("Empty Power Plant", 970, 360);
         g.drawRect(1250, 230, 290, 275);  // powerplant section 2
+        g.drawString("Empty Power Plant", 1290, 360);
         g.drawRect(1565, 230, 290, 275);  // powerplant section 3
+        g.drawString("Empty Power Plant", 1610, 360);
           
 
         g2d.setStroke(new BasicStroke(5));
@@ -84,20 +130,58 @@ public class marketPanel extends JPanel implements MouseInputListener{
         g2d.setFont(new Font("Arial", Font.BOLD, 40));
         g.drawString("Coal", 950, 580);
         g.drawImage(coal, 950, 600, 100, 100, null);
+        g2d.setFont(new Font("Arial", Font.PLAIN, 35));
+        g.drawString("Current stock: 99", 1110, 580);  // we have to change stock
+        g.drawRect(1120,600,240,100);  // button to buy resource (changes color we need to code)
+        g2d.setFont(new Font("Arial", Font.PLAIN, 30));
+        g.drawString("Buy 1 for $x", 1165, 660);
+
 
         g.drawRect(920+490, 530, 460, 200);  // oil
+        g2d.setFont(new Font("Arial", Font.BOLD, 40));
+        g.drawString("Oil", 1450, 580);
+        g.drawImage(oil, 1450, 600, 100, 100, null);
+        g2d.setFont(new Font("Arial", Font.PLAIN, 35));
+        g.drawString("Current stock: 99", 1600, 580); // we have to change stock
+        g.drawRect(1120,800,240,100);  // button to buy resource (changes color we need to code)
+        g2d.setFont(new Font("Arial", Font.PLAIN, 30));
+        g.drawString("Buy 1 for $x", 1165, 860);
+        
 
         g.drawRect(920, 740, 470, 200);  // garbage
         g2d.setFont(new Font("Arial", Font.BOLD, 40));
         g.drawString("Garbage", 925, 780);
         g.drawImage(garbage, 950, 800, 100, 100, null);
+        g2d.setFont(new Font("Arial", Font.PLAIN, 35));
+        g.drawString("Current stock: 99", 1110, 780); // we have to change stock
+        g.drawRect(1608,600,240,100);  // button to buy resource (changes color we need to code)
+        g2d.setFont(new Font("Arial", Font.PLAIN, 30));
+        g.drawString("Buy 1 for $x", 1650, 660);
 
         g.drawRect(920+490, 740, 460, 200); // uranium
+        g2d.setFont(new Font("Arial", Font.BOLD, 40));
+        g.drawString("Uranium", 1420, 780);
+        g.drawImage(uranium, 1410, 770, 150, 150, null);
+        g2d.setFont(new Font("Arial", Font.PLAIN, 35));
+        g.drawString("Current stock: 99", 1600, 780); // we have to change stock
+        g.drawRect(1608,800,240,100);  // button to buy resource (changes color we need to code)
+        g2d.setFont(new Font("Arial", Font.PLAIN, 30));
+        g.drawString("Buy 1 for $x", 1650, 860);
 
         g.drawRect(920+360, 960, 200, 70);
-        
-        // placeHolder for powerplants
+        g.setColor(new Color(64,218,53));
+        g.fillRect(920+362, 962, 196, 66);
 
+        
+        // sperators 
+        g.setColor(Color.BLACK);
+        g.drawString("Finish",920+420, 1010 );
+        g.drawLine(1100, 530, 1100, 730);
+        g.drawLine(1100, 745, 1100, 940);
+        g.drawLine(1590,530, 1590, 730);
+        g.drawLine(1590,745, 1590, 940);
+
+        
 
     }
     
